7d89a8d94e91ec3d50af9faef1920da7338495e6
Dani <danielml2003.dml@gmail.com>
2025-12-01 22:25:37 +0000

    feat: AlertRepository

--- app/src/main/java/com/pantrychef/back/data/repository/AlertRepositoryImpl.kt
+++ app/src/main/java/com/pantrychef/back/data/repository/AlertRepositoryImpl.kt
@@ -1,25 +1,49 @@
 package com.pantrychef.back.data.repository
 
 import com.pantrychef.back.data.local.dao.AlertDao
+import com.pantrychef.back.data.mapper.AlertMapper
 import com.pantrychef.back.model.Alert
 import com.pantrychef.back.repository.AlertRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class AlertRepositoryImpl(alertDao: AlertDao): AlertRepository{
-    override suspend fun getAllAlerts(): Flow<List<Alert>> {
-        TODO("Not yet implemented")
+class AlertRepositoryImpl(
+    private val alertDao: AlertDao
+): AlertRepository{
+
+
+    override fun getAllAlerts(): Flow<List<Alert>> {
+        return alertDao.getAllAlerts().map { alertsList ->
+            alertsList.map { alertEntity -> AlertMapper.entityToModel(alertEntity) }
+        }
     }
 
     override suspend fun createAlert(alert: Alert): Result<Unit> {
-        TODO("Not yet implemented")
+        return try {
+            val alertEntity = AlertMapper.modelToEntity(alert)
+            alertDao.insertAlert(alertEntity)
+            Result.success(Unit)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
     }
 
     override suspend fun dismissAlert(id: String): Result<Unit> {
-        TODO("Not yet implemented")
+        return try {
+            alertDao.deleteAlert(id)
+            Result.success(Unit)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
     }
 
     override suspend fun markAsResolved(id: String): Result<Unit> {
-        TODO("Not yet implemented")
+        return try {
+            alertDao.markAsResolved(id)
+            Result.success(Unit)
+        }catch (e: Exception){
+            Result.failure(e)
+        }
     }
 
 }
--- app/src/main/java/com/pantrychef/back/repository/AlertRepository.kt
+++ app/src/main/java/com/pantrychef/back/repository/AlertRepository.kt
@@ -4,7 +4,7 @@ import com.pantrychef.back.model.Alert
 import kotlinx.coroutines.flow.Flow
 
 interface AlertRepository {
-    suspend fun getAllAlerts(): Flow<List<Alert>>
+    fun getAllAlerts(): Flow<List<Alert>>
     suspend fun createAlert(alert: Alert): Result<Unit>
     suspend fun dismissAlert(id: String): Result<Unit>
     suspend fun markAsResolved(id: String): Result<Unit>
